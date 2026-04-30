789c98308df23429319e4a2bdf9d872cbf0c34e8
Raga Mishra <ragamishra2713@gmail.com>
2026-04-30 15:10:54 +0530

    Delete Desktop/Didasco directory

--- Desktop/Didasco/android/app/src/main/java/com/example/didasco/models/Attendance.kt
+++
@@ -1,9 +0,0 @@
-package com.example.didasco.models
-
-data class Attendance(
-    val id: String = "",
-    val studentId: String = "",
-    val date: String = "",
-    val status: String = "", // "present", "absent"
-    val method: String = "" // "manual", "qr", "device"
-)
